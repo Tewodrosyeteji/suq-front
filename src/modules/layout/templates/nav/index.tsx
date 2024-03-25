5d79ea02a1d4e02d2eb24eb1aa97eeeb841fc505
Tewodrosyeteji <teddytejines@gmail.com>
2024-03-24 23:09:12 -0700

    suq-front first commite

--- src/modules/layout/templates/nav/index.tsx
+++ src/modules/layout/templates/nav/index.tsx
@@ -4,32 +4,35 @@ import { listRegions } from "@lib/data"
 import LocalizedClientLink from "@modules/common/components/localized-client-link"
 import CartButton from "@modules/layout/components/cart-button"
 import SideMenu from "@modules/layout/components/side-menu"
+import Image from "next/image"
+import JanLogo from "../../../../../public/JANLOGO1.png"
 
 export default async function Nav() {
   const regions = await listRegions().then((regions) => regions)
 
   return (
     <div className="sticky top-0 inset-x-0 z-50 group">
-      <header className="relative h-16 mx-auto border-b duration-200 bg-white border-ui-border-base">
-        <nav className="content-container txt-xsmall-plus text-ui-fg-subtle flex items-center justify-between w-full h-full text-small-regular">
-          <div className="flex-1 basis-0 h-full flex items-center">
-            <div className="h-full">
+      <header className="relative h-16   bg-[#013B94] ">
+        <nav
+          className=" flex mx-auto max-w-7xl p-6 lg:p-6 items-center justify-between "
+          aria-label="Globla"
+        >
+          <div className="">
+            <span className="sr-only">Jan.com</span>
+            <LocalizedClientLink href="/" className="text-white ">
+              {/* <Image src={JanLogo} height={14} width={80} alt="janlogo" /> */}
+              JAN.COM
+            </LocalizedClientLink>
+          </div>
+          <div className="">
+            <div className="">
               <SideMenu regions={regions} />
             </div>
           </div>
 
-          <div className="flex items-center h-full">
-            <LocalizedClientLink
-              href="/"
-              className="txt-compact-xlarge-plus hover:text-ui-fg-base uppercase"
-            >
-              Medusa Store
-            </LocalizedClientLink>
-          </div>
-
-          <div className="flex items-center gap-x-6 h-full flex-1 basis-0 justify-end">
-            <div className="hidden small:flex items-center gap-x-6 h-full">
-              {process.env.FEATURE_SEARCH_ENABLED && (
+          <div className="flex items-center gap-x-4">
+            <div className="">
+              {/* {process.env.FEATURE_SEARCH_ENABLED && (
                 <LocalizedClientLink
                   className="hover:text-ui-fg-base"
                   href="/search"
@@ -37,20 +40,15 @@ export default async function Nav() {
                 >
                   Search
                 </LocalizedClientLink>
-              )}
-              <LocalizedClientLink
-                className="hover:text-ui-fg-base"
-                href="/account"
-              >
+              )} */}
+              <LocalizedClientLink className="" href="/account">
                 Account
               </LocalizedClientLink>
             </div>
+
             <Suspense
               fallback={
-                <LocalizedClientLink
-                  className="hover:text-ui-fg-base flex gap-2"
-                  href="/cart"
-                >
+                <LocalizedClientLink className="" href="/cart">
                   Cart (0)
                 </LocalizedClientLink>
               }
